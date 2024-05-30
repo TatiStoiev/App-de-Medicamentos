86152ac0a71386e4d2371c043d2dfda0941a55ba
TatiStoiev <tatistoiev@hotmail.com>
2024-05-30 17:30:36 -0300

    feat: commit inicial

---
+++ Backend/src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import app from './app';
+
+const PORT = 3001;
+
+const server = app.listen(PORT, () => console.log(
+  `Server is running on PORT: ${PORT}`,
+));
+
+export default server;

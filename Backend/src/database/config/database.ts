b2663db3d6d490c8d71f011a0b78cbc1c9f07fa6
TatiStoiev <tatistoiev@hotmail.com>
2024-05-31 09:50:18 -0300

    feat: adiciona arquivos de configuração do sequelize e model sequelizePaciente

---
+++ Backend/src/database/config/database.ts
@@ -0,0 +1,12 @@
+import { Options } from 'sequelize';
+
+const config: Options = {
+    username: process.env.DATABASE_USERNAME || 'root',
+    password: process.env.DATABASE_PASSWORD || 'password',
+    database: process.env.DATABASE_DATABASE || 'app_medicamentos',
+    host: process.env.DATABASE_HOST || '127.0.0.1',
+    port: Number(process.env.DATABASE_PORT) || 3306,
+    dialect: 'mysql',
+}
+
+export = config;
---
+++ Backend/src/database/models/sequelizePaciente.ts
@@ -0,0 +1,43 @@
+import {
+    DataTypes,
+    Model,
+    InferAttributes,
+    InferCreationAttributes,
+    CreationOptional,
+  } from 'sequelize';
+  import db from '.';
+
+  class sequelizePaciente extends Model<InferAttributes<sequelizePaciente>,InferCreationAttributes<sequelizePaciente>> {
+    declare id: CreationOptional<number>; 
+    declare nome: string;
+    declare email: string; 
+    declare senha: string;
+  }
+
+  sequelizePaciente.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      nome: {
+          type: DataTypes.STRING, 
+          allowNull: false,
+      }, 
+      email: {
+          type: DataTypes.STRING, 
+          allowNull: false,
+      },
+      senha: {
+          type: DataTypes.STRING, 
+          allowNull: false,
+      },
+    }, {
+        sequelize: db, 
+        modelName: 'pacientes',
+        tableName: 'pacientes',   
+  })
+
+  export default sequelizePaciente;
+

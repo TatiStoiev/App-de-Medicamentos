b423ea6e43fe444957bba2bdbc069e8656e8f2bf
TatiStoiev <tatistoiev@hotmail.com>
2024-06-01 17:15:41 -0300

    refactor: acerto de configuração

--- Backend/src/database/config/database.ts
+++ Backend/src/database/config/database.ts
@@ -4,7 +4,7 @@ const config: Options = {
     username: process.env.DATABASE_USERNAME || 'root',
     password: process.env.DATABASE_PASSWORD || 'password',
     database: process.env.DATABASE_DATABASE || 'app_medicamentos',
-    host: process.env.DATABASE_HOST || '127.0.0.1',
+    host: process.env.DATABASE_HOST,
     port: Number(process.env.DATABASE_PORT) || 3306,
     dialect: 'mysql',
 }

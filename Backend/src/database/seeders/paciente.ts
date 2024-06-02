c61806be19d29d0bab32d86b28b745ac133a1276
TatiStoiev <tatistoiev@hotmail.com>
2024-06-02 15:32:18 -0300

    Reafactor: implementação do bcrypt para senha, arquivo mock para pacientes e env.exemple

--- Backend/src/database/seeders/paciente.ts
+++ Backend/src/database/seeders/paciente.ts
@@ -1,26 +1,30 @@
 import { QueryInterface } from 'sequelize';
+const bcrypt = require('bcryptjs');
+
+const SALT_ROUNDS = process.env.BCRYPT_SALT_ROUNDS;
+
 export default {
   up: async (queryInterface: QueryInterface) => {
     await queryInterface.bulkInsert('pacientes', [
         {
             "nome": "João Silva",
             "email": "joao.silva@example.com",
-            "senha": "senha123"
+            "senha": bcrypt.hashSync('chang3m3', SALT_ROUNDS)
           },
           {
             "nome": "Maria Oliveira",
             "email": "maria.oliveira@example.com",
-            "senha": "senha456"
+            "senha": bcrypt.hashSync('chang3m3', SALT_ROUNDS)
           },
           {
             "nome": "Pedro Santos",
             "email": "pedro.santos@example.com",
-            "senha": "senha789"
+            "senha": bcrypt.hashSync('chang3m3', SALT_ROUNDS)
           },
           {
             "nome": "Ana Costa",
             "email": "ana.costa@example.com",
-            "senha": "senhaabc"
+            "senha": bcrypt.hashSync('chang3m3', SALT_ROUNDS)
           }
         ], {});
     },
